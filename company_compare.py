1c33d39740c58e034b7e16a6295907801a0d4a6f
Luque_li <dhyuguany@163.com>
2021-09-08 15:23:32 +0800

    zcfzb_update

--- company_compare.py
+++ company_compare.py
@@ -26,6 +26,25 @@ class zcfzb_quota(object):
     def __init__(self, zcfzb_df):
         self.zcfzb = zcfzb_df
 
+    @staticmethod
+    def short_term_debt_crisis(zcfzb_df):
+        '''
+        短期负债危机 = 货币资金占流动性负债的占比
+        :param zcfzb_df:
+        :return: 短期负债偿还能力占比
+        '''
+        return zcfzb_df['货币资金(万元)']/zcfzb_df['流动负债合计(万元)']
+
+    @property
+    def _liability_interest(self):
+        '''
+
+        :return: 有息负债金额
+        '''
+        self.liability_interest = self.zcfzb['长期借款(万元)'] + self.zcfzb['应付债券(万元)'] \
+                             + self.zcfzb['一年内到期的非流动负债(万元)'] + self.zcfzb['短期借款(万元)'] + self.zcfzb['交易性金融负债(万元)']
+        return self.liability_interest
+
 
 class xjllb_quota(object):
     def __init__(self, xjllb_df):
